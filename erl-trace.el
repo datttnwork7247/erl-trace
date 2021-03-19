4464b51c1c7489a295f6370e6b6c92ee85afb1fb
Dat Nguyen <dtrantie@cisco.com>
2021-03-19 15:33:44 +0700

    Update header

--- erl-trace.el
+++ erl-trace.el
@@ -1,3 +1,28 @@
+;;; erl-trace.el --- a simple package                     -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2014  Nic Ferrier
+
+;; Author: Dat <abc@xyz>
+;; Keywords: lisp
+;; Version: 1.0
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Put a description of the package here
+
 ;; TODO
 ;;
 ;; 1. erltrace-clause skipped commented code.                        DONE
@@ -92,7 +117,6 @@ end,")
     (let* ((var (thing-at-point 'symbol))
            (nstored (append erltrace-stored (list var))))
       (setq erltrace-stored nstored))))
-(global-set-key (kbd "C-c C-r") 'erltrace-store)
 
 ;; -----------------------------------------------------------------------------
 ;; INSERT
@@ -117,7 +141,6 @@ end,")
          (plutil-debug-msg "Insert %s..." "nothing")
          (erltrace-insert-string (erltrace-build-string 'nothing))
          (erltrace-goto-marked "-!-"))))
-(global-set-key (kbd "C-c C-t") 'erltrace-insert)
 
 (defun erltrace-maybe-insert-supfun ()
   "Check whether we need to insert macro or support functions."
