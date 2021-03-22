4065c67a18a06681ba45a7bb7a4bf43729ceacfd
Dat Nguyen <dtrantie@cisco.com>
2021-03-22 14:30:35 +0700

    Fix missing debug-msg

--- erl-trace.el
+++ erl-trace.el
@@ -1,9 +1,9 @@
 ;;; erl-trace.el --- a simple package                     -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2014  Nic Ferrier
+;; Copyright (C) 2021
 
-;; Author: Dat <abc@xyz>
-;; Keywords: lisp
+;; Author: MrX <abc@xyz>
+;; Keywords: erl-trace
 ;; Version: 1.0
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -20,183 +20,176 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-
 ;; Put a description of the package here
+;; Automatically insert io:format to:
+;; - Print out a atom or value of a parameter.[C-c C-t]
+;; - Store parameters for print out [C-c C-r]
+;; - Put clauses trace after `->' characters [C-e erl-trace-clause]
+;; - Put stack trace [C-e erl-trace-stack]
 
-;; TODO
-;;
-;; 1. erltrace-clause skipped commented code.                        DONE
-;; 2. erltrace-clause on detailed-level was not indented.            DONE
-;; 3. Add erltrace-stacktrace.                                       DONE
-;; 4. Replace io:format with ct:pal in .*_SUITE.erl.                 DONE
-;; 5. When in ct:pal mode, does not add ~n to the end of fmt string. DONE
-;; 6. Add debug print for C and Python.
-;;      fprintf( stdout, "%s:%d\n", __func__, __LINE__ );            -
-;; 7. Make erltrace-stacktrace work on OTP.22.                       DONE
-;; 8. Auto insert to end of function header.                         DONE
-;; 9. Do not treat macro as variable                                 -
-
-(defconst erltrace-macro
+(defconst erl-trace-macro
   "-ifndef(FUNC).
 -define(FUNC, element(2, element(2, process_info(self(), current_function)))).
--endif.")
+-endif.
+-define(iotd(A), io:format(F,A)).
+")
 
-(defconst erltrace-timestamp
-  "erltrace_timestamp() ->
+(defconst erl-trace-timestamp
+  "erl-trace_timestamp() ->
     {MS, S, US} = os:timestamp(),
     {_, {Hour, Min, Sec}} = calendar:now_to_local_time({MS, S, US}),
     MSec = trunc(US/1000),
     lists:flatten(io_lib:format(\"~2.10.0B:~2.10.0B:~2.10.0B.~3.10.0B\",
                                 [Hour, Min, Sec, MSec])).")
 
-(defconst erltrace-procinfo
-  "erltrace_process_info() ->
+(defconst erl-trace-procinfo
+  "erl-trace_process_info() ->
     proplists:get_value(registered_name, process_info(self()), self()).")
 
-(defconst erltrace-release
+(defconst erl-trace-release
   "erl -noshell -eval 'erlang:display(erlang:system_info(otp_release)), halt().'")
 
-(defconst erltrace-stacktrace
+(defconst erl-trace-stacktrace
   "try throw(exit)
 catch _:_ ->
         ST-!- = erlang:get_stacktrace(),
         ok
 end,")
 
-(defconst erltrace-stacktrace-otp-22
+(defconst erl-trace-stacktrace-otp-22
   "try throw(exit)
 catch _:_:ST-!- ->
         ok
 end,")
 
-(defvar erltrace-erlang-vsn nil)
-(defvar erltrace-level 'simple)
-(defvar erltrace-ct-pal 'false)
-(defvar erltrace-stored '())
+(defvar erl-trace-erlang-vsn nil)
+(defvar erl-trace-level 'simple)
+(defvar erl-trace-ct-pal 'false)
+(defvar erl-trace-stored '())
 
 ;; -----------------------------------------------------------------------------
 ;; LEVEL-TOGGLE
 ;; -----------------------------------------------------------------------------
-(defun erltrace-level-toggle ()
-  "Toggle erltrace between `simple' and `detail'."
-  (if (equal erltrace-level 'simple)
-      (setq erltrace-level 'detail)
-    (setq erltrace-level 'simple)))
+(defun erl-trace-level-toggle ()
+  "Toggle erl-trace between `simple' and `detail'."
+  (if (equal erl-trace-level 'simple)
+      (setq erl-trace-level 'detail)
+    (setq erl-trace-level 'simple)))
 
 ;; -----------------------------------------------------------------------------
 ;; CT-PAL-TOOGLE
 ;; -----------------------------------------------------------------------------
-(defun erltrace-ct-pal-toggle ()
-  "Toggle erltrace between `io:format' and `ct:pal'."
-  (if (equal erltrace-ct-pal 'false)
-      (setq erltrace-ct-pal 'true)
-    (setq erltrace-ct-pal 'false)))
+(defun erl-trace-ct-pal-toggle ()
+  "Toggle erl-trace between `io:format' and `ct:pal'."
+  (if (equal erl-trace-ct-pal 'false)
+      (setq erl-trace-ct-pal 'true)
+    (setq erl-trace-ct-pal 'false)))
 
 ;; -----------------------------------------------------------------------------
 ;; ADD STACKTRACE
 ;; -----------------------------------------------------------------------------
-(defun erltrace-stacktrace ()
+(defun erl-trace-stacktrace ()
   "Adding stacktrace at point."
   (let ((start-point (point)))
     (newline)
-    (if (< erltrace-erlang-vsn 22)
-        (insert erltrace-stacktrace)
-      (insert erltrace-stacktrace-otp-22))
+    (if (< erl-trace-erlang-vsn 22)
+        (insert erl-trace-stacktrace)
+      (insert erl-trace-stacktrace-otp-22))
     (save-excursion
       (goto-char start-point)
-      (erltrace-goto-marked "-!-")
-      (erltrace-insert))
+      (erl-trace-goto-marked "-!-")
+      (erl-trace-insert))
     (indent-region start-point (point))))
 
 ;; -----------------------------------------------------------------------------
 ;; STORE
 ;; -----------------------------------------------------------------------------
-(defun erltrace-store ()
+(defun erl-trace-store ()
   (interactive)
-  (when (equal (erltrace-what-at-point) 'variable)
+  (when (equal (erl-trace-what-at-point) 'variable)
     (let* ((var (thing-at-point 'symbol))
-           (nstored (append erltrace-stored (list var))))
-      (setq erltrace-stored nstored))))
+           (nstored (append erl-trace-stored (list var))))
+      (setq erl-trace-stored nstored))))
 
 ;; -----------------------------------------------------------------------------
 ;; INSERT
 ;; -----------------------------------------------------------------------------
-(defun erltrace-insert ()
+(defun erl-trace-insert ()
   (interactive)
-  (erltrace-maybe-insert-supfun)
-  (cond ((equal (erltrace-what-at-point) 'atom)
-         (plutil-debug-msg "Insert %s..." "atom")
-         (erltrace-insert-string (erltrace-build-string 'atom)))
-        ((equal (erltrace-what-at-point) 'variable)
-         (plutil-debug-msg "Insert %s..." "variable")
-         (let ((string (erltrace-build-string 'variable)))
-           (plutil-debug-msg "String %s" string))
-         (erltrace-insert-string (erltrace-build-string 'variable)))
-        ((and (equal (erltrace-what-at-point) 'nothing)
-              erltrace-stored)
-         (plutil-debug-msg "Insert %s..." "stored")
-         (erltrace-insert-string (erltrace-build-string 'stored)))
-        ((and (equal (erltrace-what-at-point) 'nothing)
-              (equal erltrace-stored nil))
-         (plutil-debug-msg "Insert %s..." "nothing")
-         (erltrace-insert-string (erltrace-build-string 'nothing))
-         (erltrace-goto-marked "-!-"))))
+  (erl-trace-maybe-insert-supfun)
+  (cond ((equal (erl-trace-what-at-point) 'atom)
+         (erl-trace-debug-msg "Insert %s..." "atom")
+         (erl-trace-insert-string (erl-trace-build-string 'atom)))
+        ((equal (erl-trace-what-at-point) 'variable)
+         (erl-trace-debug-msg "Insert %s..." "variable")
+         (let ((string (erl-trace-build-string 'variable)))
+           (erl-trace-debug-msg "String %s" string))
+         (erl-trace-insert-string (erl-trace-build-string 'variable)))
+        ((and (equal (erl-trace-what-at-point) 'nothing)
+              erl-trace-stored)
+         (erl-trace-debug-msg "Insert %s..." "stored")
+         (erl-trace-insert-string (erl-trace-build-string 'stored)))
+        ((and (equal (erl-trace-what-at-point) 'nothing)
+              (equal erl-trace-stored nil))
+         (erl-trace-debug-msg "Insert %s..." "nothing")
+         (erl-trace-insert-string (erl-trace-build-string 'nothing))
+         (erl-trace-goto-marked "-!-"))))
 
-(defun erltrace-maybe-insert-supfun ()
+(defun erl-trace-maybe-insert-supfun ()
   "Check whether we need to insert macro or support functions."
-  (erltrace-erlang-version)
+  (erl-trace-erlang-version)
   (save-excursion
-    (when (erltrace-need-macro)
-      (plutil-debug-msg "Insert %s..." "macro")
+    (when (erl-trace-need-macro)
+      (erl-trace-debug-msg "Insert %s..." "macro")
       (beginning-of-buffer)
-      (erltrace-insert-string erltrace-macro t))
-    (when (erltrace-need-supfun)
-      (plutil-debug-msg "Insert %s..." "support function")
+      (erl-trace-insert-string erl-trace-macro t))
+    (when (erl-trace-need-supfun)
+      (erl-trace-debug-msg "Insert %s..." "support function")
       (end-of-buffer)
       (newline)
-      (erltrace-insert-string erltrace-timestamp t)
+      (erl-trace-insert-string erl-trace-timestamp t)
       (goto-char (- (point) 1))
       (newline)
-      (erltrace-insert-string erltrace-procinfo t))))
+      (erl-trace-insert-string erl-trace-procinfo t))))
 
-(defun erltrace-erlang-version ()
+(defun erl-trace-erlang-version ()
   "Get and cache Erlang/OTP version."
-  (if erltrace-erlang-vsn erltrace-erlang-vsn
-    (let ((vsn (erltrace-get-erlang-version)))
-      (plutil-debug-msg "Erlang Version: %s" vsn)
-      (setq erltrace-erlang-vsn vsn))))
+  (if erl-trace-erlang-vsn erl-trace-erlang-vsn
+    (let ((vsn (erl-trace-get-erlang-version)))
+      (erl-trace-debug-msg "Erlang Version: %s" vsn)
+      (setq erl-trace-erlang-vsn vsn))))
 
-(defun erltrace-get-erlang-version ()
-  (let ((raw (shell-command-to-string erltrace-release)))
+(defun erl-trace-get-erlang-version ()
+  (let ((raw (shell-command-to-string erl-trace-release)))
     (string-match "\\\"\\(.*\\)\\\"" raw)
     (string-to-number (match-string 1 raw))))
 
-(defun erltrace-need-macro ()
-  (when (< erltrace-erlang-vsn 19)
+(defun erl-trace-need-macro ()
+  (when (< erl-trace-erlang-vsn 19)
     (beginning-of-buffer)
     (if (condition-case
             nil (search-forward "-ifndef(FUNC).")
           (error nil))
         nil t)))
 
-(defun erltrace-need-supfun ()
-  (when (equal erltrace-level 'detail)
+(defun erl-trace-need-supfun ()
+  (when (equal erl-trace-level 'detail)
     (end-of-buffer)
     (if (condition-case
-            nil (search-backward "erltrace_timestamp() ->")
+            nil (search-backward "erl-trace_timestamp() ->")
           (error nil))
         nil t)))
 
-(defun erltrace-build-string (type)
+(defun erl-trace-build-string (type)
   (cond ((equal type 'nothing)
          (let* ((fmt " -!-")
                 (args ""))
-           (erltrace-concat fmt args)))
+           (erl-trace-concat fmt args)))
         ((equal type 'atom)
          (let* ((atom (thing-at-point 'symbol))
                 (fmt (concat " " atom))
                 (args ""))
-           (erltrace-concat fmt args)))
+           (erl-trace-concat fmt args)))
         ((equal type 'variable)
          (let* ((var (thing-at-point 'symbol))
                 (fmt (if (or (string-match-p "IKeypath" var)
@@ -204,55 +197,55 @@ end,")
                          (concat " " var ": ~999p")
                        (concat "~n" var ": ~p")))
                 (args (concat ", " var)))
-           (erltrace-concat fmt args)))
+           (erl-trace-concat fmt args)))
         ((equal type 'variable)
          (let* ((var (thing-at-point 'symbol))
                 (fmt (concat "~n" var ": ~p"))
                 (args (concat ", " var)))
-           (erltrace-concat fmt args)))
+           (erl-trace-concat fmt args)))
         ((equal type 'stored)
-         (let* ((vars (delete-dups erltrace-stored))
-                (fmt (erltrace-fmt-vars vars ""))
-                (args (erltrace-args-vars vars "")))
-           (setq erltrace-stored '())
-           (erltrace-concat fmt args)))))
+         (let* ((vars (delete-dups erl-trace-stored))
+                (fmt (erl-trace-fmt-vars vars ""))
+                (args (erl-trace-args-vars vars "")))
+           (setq erl-trace-stored '())
+           (erl-trace-concat fmt args)))))
 
-(defun erltrace-concat (fmt args)
+(defun erl-trace-concat (fmt args)
   (let ((user (getenv "USER"))
-        (func (if (< erltrace-erlang-vsn 19) "?FUNC" "?FUNCTION_NAME"))
-        (printf (if (or (equal erltrace-ct-pal 'true)
+        (func (if (< erl-trace-erlang-vsn 19) "?FUNC" "?FUNCTION_NAME"))
+        (printf (if (or (equal erl-trace-ct-pal 'true)
                         (string-match ".*_SUITE.erl" (buffer-file-name)))
                     "ct:pal" "io:format"))
-        (endfmt (if (or (equal erltrace-ct-pal 'true)
+        (endfmt (if (or (equal erl-trace-ct-pal 'true)
                         (string-match ".*_SUITE.erl" (buffer-file-name)))
                     "" "~n")))
-    (if (equal erltrace-level 'detail)
+    (if (equal erl-trace-level 'detail)
         (concat printf "(\"~p:" user ":~s:~p:~p:~p:" fmt endfmt "\",\n"
-                "[erltrace_process_info(), erltrace_timestamp(),\n?MODULE, "
+                "[erl-trace_process_info(), erl-trace_timestamp(),\n?MODULE, "
                 func ", ?LINE" args "]),")
       (concat printf "(\"~p:~p:~p:" fmt endfmt "\", [?MODULE, " func ", ?LINE"
               args "]),"))))
 
-(defun erltrace-fmt-vars (vars acc)
+(defun erl-trace-fmt-vars (vars acc)
   (if (cdr vars)
       (let* ((head (car vars))
              (tail (cdr vars))
              (nacc (concat acc head ": ~p~n")))
-        (erltrace-fmt-vars tail nacc))
+        (erl-trace-fmt-vars tail nacc))
     (concat "~n" acc (car vars) ": ~p")))
 
-(defun erltrace-args-vars (vars acc)
+(defun erl-trace-args-vars (vars acc)
   (if (cdr vars)
       (let* ((head (car vars))
              (tail (cdr vars))
              (nacc (concat acc ", " head)))
-        (erltrace-args-vars tail nacc))
+        (erl-trace-args-vars tail nacc))
     (concat acc ", " (car vars))))
 
-(defun erltrace-insert-string (string &optional auto)
-  (plutil-debug-msg "Insert %s" string)
+(defun erl-trace-insert-string (string &optional auto)
+  (erl-trace-debug-msg "Insert %s" string)
   (save-excursion
-    (when (not auto) (erltrace-goto-eof-header-if-needed))
+    (when (not auto) (erl-trace-goto-eof-header-if-needed))
     (let ((start-point (point))
           (at-bol (bolp)))
       ;; If we are not standing at beginning-of-line
@@ -261,11 +254,11 @@ end,")
       (when at-bol (newline))
       (indent-region start-point (point)))))
 
-(defun erltrace-goto-marked (string)
+(defun erl-trace-goto-marked (string)
   (when (condition-case nil (search-forward string) (error nil))
     (delete-backward-char (length string))))
 
-(defun erltrace-what-at-point ()
+(defun erl-trace-what-at-point ()
   "Returns `variable' `atom' or `nothing' at cursor."
   (let ((thing (thing-at-point 'symbol))
         (case-fold-search nil))
@@ -274,48 +267,74 @@ end,")
             'variable 'atom)
       'nothing)))
 
-(defun erltrace-func-header-p ()
+(defun erl-trace-func-header-p ()
   (save-excursion
     (let ((current (point))
-          (begin (progn (erltrace-boc-safe) (point)))
+          (begin (progn (erl-trace-boc-safe) (point)))
           (end (search-forward "->")))
       (if (and (<= begin current) (< current end)) t nil))))
 
-(defun erltrace-boc-safe ()
+(defun erl-trace-boc-safe ()
   ;; We are not stand at beginning of function clause,
   ;; Lets move to it.
   (unless (erlang-get-function-name) (erlang-beginning-of-clause)))
 
-(defun erltrace-goto-eof-header-if-needed ()
+(defun erl-trace-goto-eof-header-if-needed ()
   ;; We are standing in the middle of function header,
   ;; Lets move to end of function header.
-  (when (erltrace-func-header-p) (search-forward "->")))
+  (when (erl-trace-func-header-p) (search-forward "->")))
 
 ;; -----------------------------------------------------------------------------
 ;; CLAUSE
 ;; -----------------------------------------------------------------------------
 ;; Increase the number of lisp bindings.
 (setq max-specpdl-size 13000)
-(defun erltrace-clause ()
+(defun erl-trace-clause ()
   (save-excursion
-    (erltrace-maybe-insert-supfun)
+    (erl-trace-maybe-insert-supfun)
     (erlang-end-of-function)
     (let ((stop-point (point)))
       (erlang-beginning-of-function)
       (let ((start-point (point)))
-        (erltrace-clause-loop stop-point 1)
+        (erl-trace-clause-loop stop-point 1)
         (indent-region start-point (point))))))
 
-(defun erltrace-clause-loop (stop number)
+(defun erl-trace-clause-loop (stop number)
   (search-forward "->")
   (let ((current (point)))
     (if (nth 4 (syntax-ppss))
-        (erltrace-clause-loop stop number)
+        (erl-trace-clause-loop stop number)
       (let* ((fmt (concat " case: " (number-to-string number)))
-             (str (erltrace-concat fmt "")))
+             (str (erl-trace-concat fmt "")))
         (when (< current stop)
           (newline-and-indent)
           (insert str)
-          (erltrace-clause-loop (+ (- (point) current) stop) (+ number 1)))))))
+          (erl-trace-clause-loop (+ (- (point) current) stop) (+ number 1)))))))
+
+;; -----------------------------------------------------------------------------
+;; RUN-COMMAND
+;; -----------------------------------------------------------------------------
+(defun erl-trace-run-cmd ()
+  (interactive)
+  (let ((command (completing-read "Run erl-trace: " '("erl-trace-level"
+                                                      "erl-trace-ctpal"
+                                                      "erl-trace-clause"
+                                                      "erl-trace-stacktrace"))))
+    (cond ((equal command "run")                 (erl-trace-run-test))
+          ((equal command "erl-trace-level")      (erl-trace-level-toggle))
+          ((equal command "erl-trace-ctpal")      (erl-trace-ct-pal-toggle))
+          ((equal command "erl-trace-clause")     (erl-trace-clause))
+          ((equal command "erl-trace-stacktrace") (erl-trace-stacktrace))
+          )))
+
+;; -----------------------------------------------------------------------------
+;; DEBUG-MSG
+;; -----------------------------------------------------------------------------
+(defvar erl-trace-debug nil
+  "Control whether printing debug messages.")
+
+(defun erl-trace-debug-msg (format &rest args)
+  "Debug message."
+  (when erl-trace-debug (apply 'message (add-to-list 'args format))))
 
 (provide 'erl-trace)
