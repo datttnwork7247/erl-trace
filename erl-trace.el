ef305145a77a4f0e398f5532c3224cc50b6bf09d
Dat Nguyen <dtrantie@cisco.com>
2021-10-25 13:54:37 +0700

    remove redundant cmpload commands

--- erl-trace.el
+++ erl-trace.el
@@ -451,10 +451,6 @@ end,")
   (global-set-key (kbd "C-c C-t") 'erl-trace-insert)
   (global-set-key (kbd "C-c C-e") 'erl-trace-run-cmd)
   (global-set-key (kbd "C-c C-r") 'erl-trace-store)
-
-  (global-set-key [f6] 'cmpload-compile-and-load)
-  (global-set-key [f12] 'cmpload-compile-and-load-f12)
-  (global-set-key [\C-f12] 'cmpload-compile-and-load-cf12)
   )
 (erl-trace-key-bindings)
 
